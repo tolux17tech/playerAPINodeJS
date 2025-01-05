65c21d96da0b4f24dba417fbb9205c9d5ede82a6
Tolulope Gbadamosi <tolux17.tech@gmail.com>
2025-01-05 09:48:19 -0500

    added full api features

--- Controllers/playersController.js
+++ Controllers/playersController.js
@@ -3,7 +3,13 @@ const Player = require('../model/playersModel');
 
 const players = JSON.parse(fs.readFileSync(`${__dirname}/../data/players.json`, 'utf-8'));
 
+exports.aliasTopPlayers = (req, res, next) => {
+    req.query.limit = 5;
+    req.query.sort = '-numberOfGoals,rating';
+    req.query.fields = "name, country, rating, numberOfGoals, country";
+    next();
 
+}
 exports.checkId = (req, res, next, val) => {
 
     // if (val > 1000) {
@@ -23,21 +29,71 @@ exports.checkBody = (req, res, next) => {
     next();
 }
 
+
+
 exports.getAllPlayers = async (req, res) => {
     try {
-        const data = await Player.find();
+        let newQuery = { ...req.query };
+
+        //Fields to exclude
+        const excludedQuery = ['sort', 'limit', 'fields', 'page'];
+        excludedQuery.forEach(field => delete newQuery[field])
+
+
+        //Advanced Filtering
+        let queryString = JSON.stringify(newQuery);
+        queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+
+        // Query
+        let data = Player.find(JSON.parse(queryString));
+
+        //Cherry Picking
+        if (req.query.fields) {
+            console.log(req.query.fields);
+            data.select(req.query.fields.split(',').join(' '))
+        }
+        //Sorting fields
+        if (req.query.sort) {
+
+            data = data.sort(req.query.sort.split(',').join(' '))
+        }
+        else {
+            data = data.sort('createdAt')
+        }
+
+        //Limiting the number of documents
+        if (req.query.limit) {
+            data = data.limit(req.query.limit)
+        }
+
+        //Pagination
+        const page = +req.query.page || 1;
+        const limit = +req.query.limit || 100;
+        const skip = (page - 1) * limit;
+
+        data = data.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const numPlayers = await Player.countDocuments();
+            if ((skip) >= numPlayers) {
+                throw new Error('Limit your search by asking for only few pages')
+            }
+        }
+
+        //Final Data
+        const players = await data;
 
         res.status(200).json({
             status: 'success',
-            results: data.length,
+            results: players.length,
             data: {
-                data
+                players
             }
         })
     } catch (error) {
         console.log(error);
         res.status(404).json({
-            status: error,
+            status: error.message,
             message: "fail"
         })
     }
--- Routers/playerRoute.js
+++ Routers/playerRoute.js
@@ -5,6 +5,9 @@ const playerRouter = express.Router();
 
 playerRouter.param("id", playerController.checkId)
 
+playerRouter.route('/top-5-players')
+    .get(playerController.aliasTopPlayers, playerController.getAllPlayers);
+
 playerRouter.route("/")
     .get(playerController.getAllPlayers)
     .post(playerController.checkBody, playerController.addPlayer);
